1f960e4b0a784c5d2218754b8b9d7eb85b208d44
Try <alekcandr.patalieta@gmail.com>
2023-08-11 14:51:12 +0300

    done towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let sortNumb = [];
+
+  if(matrix && matrix.length > 0) {
+    for(let arr of matrix) {
+      matrix.indexOf(arr) % 2
+        ? sortNumb = [...sortNumb, ...arr.sort((a, b) => b-a)]
+        : sortNumb = [...sortNumb, ...arr]
+    }
+  }
+  return sortNumb;
 }
